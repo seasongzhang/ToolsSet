a7d67534725dff09ef08c7e5198574ac81387196
Seasong@EIS-846 <seasong.zhang@outlook.com>
2016-03-11 17:16:59 +0800

    test __init__ complete.

--- xparts_update/test/__init__.py
+++ xparts_update/test/__init__.py
@@ -1,15 +1,15 @@
-import filecmp
 import codecs
 import os
 import shutil
-import datetime
+import time
 
-TEST_BASE_DIR = "test"
+TEST_BASE_DIR = r"..\test"
 TEST_DATA_DIR = os.path.join(TEST_BASE_DIR, "data")
 
 
 def setup():
     print("Package setup.")
+
     # Create src and dst directories
     if os.path.exists(TEST_DATA_DIR):
         shutil.rmtree(TEST_DATA_DIR)
@@ -20,12 +20,62 @@ def setup():
     os.mkdir(dst_dir)
 
     # Create same files in both src and dst
-    codecs.open(os.path.join(src_dir, "A.txt"), "w", encoding="utf-8").write("A.txt")
-    shutil.cop(os.path.join(src_dir, "A.txt"), dst_dir)
-    metadata_src = os.stat(os.path.join(src_dir, "A.txt"))
-    metadata_dst = os.stat(os.path.join(dst_dir, "A.txt"))
-    filecmp.cmp
+    codecs.open(os.path.join(src_dir, "A.txt"), "w").write("A.txt")
+    shutil.copy2(os.path.join(src_dir, "A.txt"), dst_dir)
+
+    # Create file in src but not in dst
+    codecs.open(os.path.join(src_dir, "B.txt"), "w").write("B.txt")
+
+    # Create file in dst but not in src
+    codecs.open(os.path.join(dst_dir, "C.txt"), "w").write("C.txt")
+
+    # Create file in src and changed in dst
+    codecs.open(os.path.join(src_dir, "D.txt"), "w").write("D.txt")
+    shutil.copy2(os.path.join(src_dir, "D.txt"), dst_dir)
+    time.sleep(0.001)
+    codecs.open(os.path.join(dst_dir, "D.txt"), "a").write("D.txt")
+
+    # Create file in dst and changed in src
+    codecs.open(os.path.join(dst_dir, "E.txt"), "w").write("E.txt")
+    shutil.copy2(os.path.join(dst_dir, "E.txt"), src_dir)
+    time.sleep(0.001)
+    codecs.open(os.path.join(src_dir, "E.txt"), "a").write("E.txt")
+
+    # Create same dirs both in src and dst
+    os.mkdir(os.path.join(src_dir, "AA"))
+    os.mkdir(os.path.join(dst_dir, "AA"))
+    codecs.open(os.path.join(src_dir, "AA\A.txt"), "w").write("A.txt")
+    shutil.copy2(os.path.join(src_dir, "AA\A.txt"), os.path.join(dst_dir, "AA"))
+
+    # Create dir in src but not in dst
+    os.mkdir(os.path.join(src_dir, "BB"))
+    codecs.open(os.path.join(src_dir, "BB\B.txt"), "w").write("B.txt")
+
+    # Create dir in src but not in dst
+    os.mkdir(os.path.join(dst_dir, "CC"))
+    codecs.open(os.path.join(dst_dir, "CC\C.txt"), "w").write("C.txt")
+
+    # Create dir in src and changed in dst
+    os.mkdir(os.path.join(src_dir, "DD"))
+    codecs.open(os.path.join(src_dir, r"DD\D.txt"), "w").write("D.txt")
+    shutil.copytree(os.path.join(src_dir, "DD"), os.path.join(dst_dir, "DD"))
+    codecs.open(os.path.join(dst_dir, r"DD\D.txt"), "a").write("D.txt")
+
+    # Create dir in dst and changed in src
+    os.mkdir(os.path.join(dst_dir, "EE"))
+    codecs.open(os.path.join(dst_dir, r"EE\E.txt"), "w").write("E.txt")
+    shutil.copytree(os.path.join(dst_dir, "EE"), os.path.join(src_dir, "EE"))
+    codecs.open(os.path.join(src_dir, r"EE\E.txt"), "a").write("E.txt")
+
 
 def teardown():
     print("Package teardown.")
-    # shutil.rmtree(TEST_DATA_DIR)
+    if os.path.exists(TEST_DATA_DIR):
+        shutil.rmtree(TEST_DATA_DIR)
+
+
+try:
+    setup()
+except:
+    pass
+teardown()
--- xparts_update/xparts_update.py
+++ xparts_update/xparts_update.py
@@ -1,24 +1,23 @@
 import os
 import shutil
-
-"""
-    This script is used for:
-    1. check whether directory r"J:\\UGNX\\NX_LIB", including all the files
-    and dirs inside, has been updated. If YES, find the ones which
-
-    testdata design:
+import codecs
 
 
-    需要一个增量式的log，记录每一次的变更
-
-    Functions:
-    1. Read dirs and files information;
-    2. For dirs or files that exist in src but not in dst, record into add_list;
-    3. For files that both exist in src and dst, compare the update time,
-       if not consist, record into diff_list with dir's name;
-    4. For dirs that both exist in src and dst, step into 2.
-
-"""
+# This script is used for:
+# 1. check whether directory r"J:\\UGNX\\NX_LIB", including all the files
+# and dirs inside, has been updated. If YES, find the ones which
+#
+# testdata design:
+#
+#
+# 需要一个增量式的log，记录每一次的变更
+#
+# Functions:
+# 1. Read dirs and files information;
+# 2. For dirs or files that exist in src but not in dst, record into add_list;
+# 3. For files that both exist in src and dst, compare the update time,
+#    if not consist, record into diff_list with dir's name;
+# 4. For dirs that both exist in src and dst, step into 2.
 
 
 class Log:
@@ -37,29 +36,43 @@ class Log:
             file_path = os.path.join(root, f)
             if not (os.path.exists(self.src_to_dst(file_path))):
                 self.log_not_in_dst.append(file_path)
-            elif os.stat(file_path)[8] > os.stat(self.src_to_dst(file_path))[8]:
+            elif os.stat(file_path).st_mtime_ns > os.stat(self.src_to_dst(file_path)).st_mtime_ns:
                 self.log_src_is_newer.append(file_path)
         for d in dirs:
             self.walk(os.path.join(root, d))
 
     def show(self):
-        for x in self.log_not_in_dst:
-            print(x)
-        for x in self.log_src_is_newer:
-            print(x)
+        with codecs.open("xparts_log.txt", "w", encoding='utf-8') as f:
+            print("These files are in src but not in dst.")
+            f.write("These files are in src but not in dst.\r\n")
+            for x in self.log_not_in_dst:
+                print(x)
+                f.write(x + "\r\n")
+            print()
+            f.write("\r\n  ")
+            print("These files are newer src than in dst.")
+            f.write("These files are newer src than in dst.\r\n")
+            for x in self.log_src_is_newer:
+                print(x)
+                f.write(x + "\r\n")
 
-    def exec(self):
+    def do(self):
         for f in self.log_src_is_newer:
-            shutil.copyfile(f, self.src_to_dst(f))
+            try:
+                shutil.copyfile(f, self.src_to_dst(f))
+            except PermissionError:
+                os.chmod(self.src_to_dst(f), )
             print("Copy " + f + " from src to dst.")
 
         for f in self.log_not_in_dst:
             shutil.copyfile(f, self.src_to_dst(f))
             print("Copy " + f + " from src to dst.")
 
+
 if __name__ == "__main__":
-    dir_src = r"C:\SeaGit\ToolsSet\xparts_update\testdata\src"
-    dir_dst = r"C:\SeaGit\ToolsSet\xparts_update\testdata\dst"
-    log = Log(dir_src, dir_dst)
-    log.walk(dir_src)
-    log.exec()
+    ds = r"D:\Backup\NX_LIB"
+    dd = r"D:\Backup\XParts"
+    log = Log(ds, dd)
+    log.walk(ds)
+    log.show()
+    log.do()
